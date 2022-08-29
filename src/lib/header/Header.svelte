2011eb4ee0ddcf07372efcaae0fbf82ffa72bf9c
Jochem Dobber <45901428+joggjogg@users.noreply.github.com>
2022-08-29 16:40:00 +0200

    Add hero section to index route

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -26,6 +26,7 @@
 	header {
 		display: flex;
 		justify-content: space-between;
+		padding: 2em 4em;
 	}
 
 	.corner {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,46 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Jochem Dobber personal website" />
 </svelte:head>
 
-<section>
-	<h1>Hi, my name is Jochem and I'm a Olympic Medalist.</h1>
+<section class="hero">
+	<h1>Hi, my name is Jochem and I'm an <span>Olympic Medalist</span>.</h1>
+	<div class="image">
+        <img src="hero.png" alt="Jochem Dobber relay" />
+    </div>
 </section>
 
 <style>
-	section {
+	.hero {
+		background-color: var(--background);
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		height: 80vh;
+		margin: 0 4em;
+		padding: 4em;
+		box-sizing: border-box;
+		border-radius: 10px;
 	}
+
+	h1 {
+		flex: 1 0 0;
+		text-align: left;
+	}
+
+	span {
+		color: var(--accent-color);
+	}
+
+	.image {
+        margin-top: -5em;
+		margin-bottom: -4em;
+        width: 60%;
+    }
+
+    img {
+        height: 100%;
+        object-fit: fill;
+    }
 </style>
